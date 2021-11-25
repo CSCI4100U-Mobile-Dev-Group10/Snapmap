20e61f2e8772292098be5444ff549858dc45f112
XavierChanth <xchanthavong@gmail.com>
2021-11-24 21:29:08 -0500

    clean up auth_service

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,65 +1,50 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:snapmap/globals.dart';
-
-// this file holds functions for sign up and authorization when loggin in
-// the functions interact with the firestore
+import 'package:snapmap/models/user.dart';
+import 'package:snapmap/services/user_service.dart';
+import 'package:snapmap/utils/logger.dart';
 
 final users = FirebaseFirestore.instance.collection("Users");
 
-
 Future<bool> authUser(Map data) async {
-  bool returnValue = true;
-  var dict;
+  Map<String, dynamic>? dict;
 
-  await users.doc(data['username']).get().then((DocumentSnapshot snapshot) {
-    try {
-      dict = snapshot.data() as Map;
-    } on StateError catch (e) {
-      print('No nested field exists!');
-    }
+  try {
+    DocumentSnapshot<Map<String, dynamic>> doc =
+        await users.doc(data['username']).get();
+    dict = doc.data();
+  } on StateError {
+    logger.e('No nested field exists!');
+    return false;
+  } catch (e) {
+    logger.e(e);
+    return false;
+  }
 
-    if (snapshot.data() != null) {
-      user.username = data['username'];
-      user.email = dict['email'];
-      user.password = data['password'];
-      user.profileURL = dict['profileURL'];
-      user.displayName = dict['display_name'];
-      if (data['password'] != dict['password']) {
-        returnValue = false;
-      }
-    } else {
-      returnValue = false;
-    }
-  }).catchError((error) {
-    returnValue = false;
-  });
-  return returnValue;
+  if (dict == null) return false;
+  if (data['password'] != dict['password']) return false;
+  UserService.getInstance().setUser(User.fromMap(dict));
+  return true;
 }
 
-Future<String> signUp(Map data) async {
-  String returnValue = 'true';
-  await users.doc(data['username']).get().then((value) async {
+Future<bool> signUp(Map data) async {
+  try {
+    DocumentSnapshot<Map<String, dynamic>> userDoc =
+        await users.doc(data['username']).get();
+
+    QuerySnapshot<Map<String, dynamic>> emailDoc =
+        await users.where('email', isEqualTo: data['email']).get();
+
+    if (emailDoc.docs.isNotEmpty) return false;
+    if (userDoc.data() == null) return false;
+
     await users
-        .where('email', isEqualTo: data['email'])
-        .get()
-        .then((emailInstance) {
-      if (emailInstance.docs.isNotEmpty) {
-        returnValue = 'email';
-      } else {
-        if (value.data() != null) {
-          returnValue = 'username';
-        } else if (data['conPass'] == data['password']) {
-          users.doc(data['username']).set({
-            'email': data['email'],
-            'password': data['password']
-          }).then((value) {
-            print("Added User");
-          }).catchError((error) => print("Failed to add User: $error"));
-        } else {
-          returnValue = 'password';
-        }
-      }
-    });
-  });
-  return returnValue;
+        .doc(data['username'])
+        .set({'email': data['email'], 'password': data['password']});
+
+    logger.i('Added User => ${data['username']}');
+  } catch (e) {
+    logger.e(e);
+    return false;
+  }
+  return true;
 }
