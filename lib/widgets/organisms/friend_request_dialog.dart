515da7e1de3584de153606ad50af1d5759e55b3c
XavierChanth <xchanthavong@gmail.com>
2021-12-01 13:35:20 -0500

    Update dialogs with modal spinners

--- lib/widgets/organisms/friend_request_dialog.dart
+++ lib/widgets/organisms/friend_request_dialog.dart
@@ -3,15 +3,29 @@ import 'package:snapmap/models/user.dart';
 import 'package:snapmap/services/user_service.dart';
 import 'package:snapmap/widgets/molecules/profile_header.dart';
 
-class FriendRequestDialog extends StatelessWidget {
+class FriendRequestDialog extends StatefulWidget {
   const FriendRequestDialog(this.username, {Key? key}) : super(key: key);
   final String username;
 
   @override
+  State<FriendRequestDialog> createState() => _FriendRequestDialogState();
+}
+
+class _FriendRequestDialogState extends State<FriendRequestDialog> {
+  bool isAccepted = false;
+
+  @override
   Widget build(BuildContext context) {
+    if (isAccepted) {
+      return const Dialog(
+        backgroundColor: Colors.transparent,
+        child: Center(child: CircularProgressIndicator()),
+      );
+    }
+
     return AlertDialog(
       title: FutureBuilder(
-        future: UserService.getInstance().getOtherUser(username),
+        future: UserService.getInstance().getOtherUser(widget.username),
         builder: (BuildContext context, AsyncSnapshot snapshot) {
           if (snapshot.hasData) {
             User? user = snapshot.data;
@@ -22,7 +36,7 @@ class FriendRequestDialog extends StatelessWidget {
           return const LinearProgressIndicator();
         },
       ),
-      content: Text('Are you sure you want to add $username?'),
+      content: Text('Are you sure you want to add ${widget.username}?'),
       actions: [
         IconButton(
           icon: const Icon(Icons.cancel_rounded, color: Colors.redAccent),
@@ -33,8 +47,13 @@ class FriendRequestDialog extends StatelessWidget {
         IconButton(
           icon: const Icon(Icons.check, color: Colors.green),
           onPressed: () async {
-            await UserService.getInstance().requestFriend(username);
-            Navigator.pop(context);
+            if (!isAccepted) {
+              setState(() {
+                isAccepted = true;
+              });
+              await UserService.getInstance().requestFriend(widget.username);
+              Navigator.pop(context);
+            }
           },
         ),
       ],
--- lib/widgets/organisms/post_confirmation_dialog.dart
+++ lib/widgets/organisms/post_confirmation_dialog.dart
@@ -3,15 +3,28 @@ import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:snapmap/services/post_service.dart';
 
-class PostConfirmationDialog extends StatelessWidget {
+class PostConfirmationDialog extends StatefulWidget {
   const PostConfirmationDialog(this.bytes, {Key? key}) : super(key: key);
   final Uint8List bytes;
 
   @override
+  State<PostConfirmationDialog> createState() => _PostConfirmationDialogState();
+}
+
+class _PostConfirmationDialogState extends State<PostConfirmationDialog> {
+  bool isAccepted = false;
+
+  @override
   Widget build(BuildContext context) {
+    if (isAccepted) {
+      return const Dialog(
+        backgroundColor: Colors.transparent,
+        child: Center(child: CircularProgressIndicator()),
+      );
+    }
     return AlertDialog(
       title: const Text('Are you sure you want to post this?'),
-      content: Image.memory(bytes),
+      content: Image.memory(widget.bytes),
       actions: [
         IconButton(
           icon: const Icon(Icons.cancel_rounded, color: Colors.redAccent),
@@ -22,8 +35,13 @@ class PostConfirmationDialog extends StatelessWidget {
         IconButton(
           icon: const Icon(Icons.check, color: Colors.green),
           onPressed: () async {
-            await PostService.getInstance().uploadPost(bytes);
-            Navigator.pop(context);
+            if (!isAccepted) {
+              setState(() {
+                isAccepted = true;
+              });
+              await PostService.getInstance().uploadPost(widget.bytes);
+              Navigator.pop(context);
+            }
           },
         ),
       ],
