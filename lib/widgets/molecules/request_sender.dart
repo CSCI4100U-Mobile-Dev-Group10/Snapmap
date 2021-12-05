0dcdbfabf8d5d7f80df297f48ff6dee21cc1f20a
XavierChanth <xchanthavong@gmail.com>
2021-12-05 18:34:08 -0500

    center the request sender

--- lib/widgets/molecules/request_sender.dart
+++ lib/widgets/molecules/request_sender.dart
@@ -3,7 +3,7 @@ import 'package:snapmap/services/user_service.dart';
 import 'package:snapmap/utils/logger.dart';
 
 class FriendRequestSender extends StatefulWidget {
-  const FriendRequestSender({ Key? key }) : super(key: key);
+  const FriendRequestSender({Key? key}) : super(key: key);
 
   @override
   _FriendRequestSenderState createState() => _FriendRequestSenderState();
@@ -13,11 +13,10 @@ class _FriendRequestSenderState extends State<FriendRequestSender> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _controller = new TextEditingController();
   String usernameToSend = '';
-  bool  flag = false;
+  bool flag = false;
 
   checkifUserExists(String us) async {
-    var result =
-        await UserService.getInstance().getOtherUser(us);
+    var result = await UserService.getInstance().getOtherUser(us);
     // verify user exists
     if (result == null) return false;
     return true;
@@ -33,45 +32,47 @@ class _FriendRequestSenderState extends State<FriendRequestSender> {
           fit: StackFit.loose,
           children: [
             Positioned(
-              left: (MediaQuery.of(context).size.width) * 0.01,
+              left: (MediaQuery.of(context).size.width) * 0.03,
               top: 5,
               width: (MediaQuery.of(context).size.width) * 0.78,
               child: TextFormField(
-                  controller: _controller,
-                  decoration: const InputDecoration(
-                    labelText: 'Send a friend request',
-                    // border: OutlineInputBorder(),
-                    hintText: 'Enter username (e.g Andrew)',
-                  ),
-                  onChanged: (text) async {
-                    await checkifUserExists(text).then((value) {
-                      flag = value;
-                    });
-                  },
-                  validator: (value) {
-                    var _user = UserService.getInstance().getCurrentUser()!;
-                    if (value == null || value.isEmpty) {
-                      return 'Must enter username';
-                    } else if (!flag) {
-                      return 'User does not exist';
-                    } else if (_user.friends.contains(value.toString())) {
-                      return 'You are already friends with ${value.toString()}';
-                    } else if (_user.sentFriendRequests.contains(value.toString())) {
-                      return 'You have already sent a request to ${value.toString()}';
-                    } else if (_user.receivedFriendRequests.contains(value.toString())) {
-                      return 'You have already recieved a request from ${value.toString()}';
-                    } else if (_user.username == value.toString()) {
-                      return 'You cannot request yourself silly';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {
-                    usernameToSend = value.toString();
-                  },
+                controller: _controller,
+                decoration: const InputDecoration(
+                  labelText: 'Send a friend request',
+                  // border: OutlineInputBorder(),
+                  hintText: 'Enter username (e.g Andrew)',
                 ),
+                onChanged: (text) async {
+                  await checkifUserExists(text).then((value) {
+                    flag = value;
+                  });
+                },
+                validator: (value) {
+                  var _user = UserService.getInstance().getCurrentUser()!;
+                  if (value == null || value.isEmpty) {
+                    return 'Must enter username';
+                  } else if (!flag) {
+                    return 'User does not exist';
+                  } else if (_user.friends.contains(value.toString())) {
+                    return 'You are already friends with ${value.toString()}';
+                  } else if (_user.sentFriendRequests
+                      .contains(value.toString())) {
+                    return 'You have already sent a request to ${value.toString()}';
+                  } else if (_user.receivedFriendRequests
+                      .contains(value.toString())) {
+                    return 'You have already recieved a request from ${value.toString()}';
+                  } else if (_user.username == value.toString()) {
+                    return 'You cannot request yourself silly';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  usernameToSend = value.toString();
+                },
+              ),
             ),
             Positioned(
-              left: (MediaQuery.of(context).size.width) * 0.81,
+              left: (MediaQuery.of(context).size.width) * 0.83,
               top: 5,
               width: (MediaQuery.of(context).size.width) * 0.15,
               height: 57,
@@ -80,9 +81,11 @@ class _FriendRequestSenderState extends State<FriendRequestSender> {
                   if (_formKey.currentState!.validate()) {
                     _formKey.currentState!.save();
                     _controller.clear();
-                    var value = await UserService.getInstance().getOtherUser(usernameToSend);
+                    var value = await UserService.getInstance()
+                        .getOtherUser(usernameToSend);
                     if (value != null) {
-                      await UserService.getInstance().requestFriend(usernameToSend);
+                      await UserService.getInstance()
+                          .requestFriend(usernameToSend);
                     } else {
                       logger.i('user does not exist');
                     }
